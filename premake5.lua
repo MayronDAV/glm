98eece6d6c13d73e0b8d355fddbbe3cb779976a3
MayronDAV <118127551+MayronDAV@users.noreply.github.com>
2024-07-09 18:05:06 -0300

    minor changes
    added premake5
    moved the files to the include folder

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "glm"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/Thirdparty/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/Thirdparty/%{prj.name}")
+
+
+	files
+	{
+		"include/**.h",
+        "include/**.hpp",
+        "include/**.inl"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
